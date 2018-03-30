47e38eab1c6848c3450356a552105a722932a79c
RicoFio <s1650091@sms.ed.ac.uk>
2018-03-30 14:06:57 +0100

    something done

--- main.py
+++ main.py
@@ -5,25 +5,37 @@ import numpy as np
 
 def getData(data):
 
-    size = []
-    data_separation = np.zeros((1,3))
-    tracker = 2
+    data_separation = np.zeros((0,3))
 
     for idx, row in enumerate(data.iter_rows(min_row=1)):
-	new_data = np.array([row[0].value,row[1].value,row[2].value])
+        new_data = np.array([row[0].value,row[1].value,row[2].value])
         if type(new_data[0]) == np.float64:
-		#print new_data
-		data_separation = np.vstack([data_separation,new_data])  
-		#print "added"		
-	else:
-	    print data_separation.size
-	    size.append(data_separation.size)
-	    print(new_data[0])
+    	    #print new_data
+            data_separation = np.vstack([data_separation,new_data])
+    	    #print "added"
 
-    size = np.unique(size)  
-    print data_separation[0:size[1],]
-    print np.unique(size)
-    return data_separation, size
+    return data_separation
+
+def calc_diff_and_norm(m,dp,c):
+    print m.shape
+    r = (np.subtract(m[:,0],m[:,1])).T
+    #print r.size
+    f = r.copy()
+    out = m.copy()
+
+    for j in range(4):
+        for i in range(c):
+            minval = np.min(f[j*i*dp:(i+1)*dp*(j+1)])
+            f[j*i*dp:(i+1)*dp*(j+1)] = np.subtract(f[j*i*dp:(i+1)*dp*(j+1)],minval)
+
+    #print r
+    #print f
+    #print out
+    m[:,:-1] = r
+    m[:,:-1] = f
+
+    return m
+
 
 """Collect command-line options in a dictionary"""
 
@@ -36,7 +48,7 @@ def getopts(argv):
     return opts
 
 if __name__ == '__main__':
-    
+
     myargs = getopts(argv)
 
     # load filename
@@ -44,17 +56,17 @@ if __name__ == '__main__':
     # load workbook
     wb = load_workbook(filename = file)
     # retrieve number of comets
-    comets = myargs['-c']
+    comets = int(myargs['-c'])
+    # retrieve number of datapoints
+    data_points = int(myargs['-dp'])
     # retrieve first sheet name
     sheet_name = wb.sheetnames[0]
     # load data from sheet
     data = wb.active
     # obtain matrices by iterating over it
-    matrices, size = getData(data)
+    matrices = getData(data)
+    # calculate differences and the normalize differences
+    differencen, normalized = calc_diff_and_norm(matrices, data_points, comets)
+    #
 
     #print matrices[1:size]
-
-
-
-
-
